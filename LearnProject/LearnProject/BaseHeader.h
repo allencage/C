24019b96c6630d70c966d600b078095fd91e1874
Alin <alingabriel.ciuca@gmail.com>
2016-12-04 19:05:52 +0200

    Added BaseHeader

---
+++ LearnProject/LearnProject/BaseHeader.h
@@ -0,0 +1,18 @@
+//
+//  BaseHeader.h
+//  LearnProject
+//
+//  Created by MacBookPro on 12/4/16.
+//  Copyright © 2016 MacBookPro. All rights reserved.
+//
+
+#ifndef BaseHeader_h
+#define BaseHeader_h
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <math.h>
+#include<string.h>
+#include "GenericMessages.h"
+
+#endif /* BaseHeader_h */
