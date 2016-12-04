65c2255c615e99e07ba7ec999dff63d35ed0e086
Alin <alingabriel.ciuca@gmail.com>
2016-12-04 19:07:23 +0200

    Added GenericMessages

---
+++ LearnProject/LearnProject/GenericMessages.h
@@ -0,0 +1,14 @@
+//
+//  GenericMessages.h
+//  LearnProject
+//
+//  Created by MacBookPro on 12/4/16.
+//  Copyright © 2016 MacBookPro. All rights reserved.
+//
+
+#ifndef GenericMessages_h
+#define GenericMessages_h
+
+#define GENERICMSG "Give input: "
+
+#endif /* GenericMessages_h */
