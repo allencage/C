52a065675d0ee81be62078ed175f75bac397712f
Alin <alingabriel.ciuca@gmail.com>
2016-12-04 19:08:36 +0200

    Added InputValidation.h

---
+++ LearnProject/LearnProject/InputValidation.h
@@ -0,0 +1,18 @@
+//
+//  InputValidation.h
+//  LearnProject
+//
+//  Created by MacBookPro on 12/4/16.
+//  Copyright © 2016 MacBookPro. All rights reserved.
+//
+
+#ifndef InputValidation_h
+#define InputValidation_h
+
+#include "BaseHeader.h"
+
+void ValidateInput();
+int GetInt(char message[50]);
+int *GetIntPointer(char message[50]);
+
+#endif /* InputValidation_h */
